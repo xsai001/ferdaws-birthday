3170e755438eeb416197481c399bbca66bd2e2e8
xsai001 <tamalmoke@gmail.com>
2026-07-15 02:24:32 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-const msg=`بنتي الحبيبة 🤍، باباك كيبغيك بزاف. ما تتصوريش شحال أنا فرحان حيث نتي معايا. والله كنتمنا ليك عيد ميلاد زوين، وإن شاء الله المرة الجاية غادي نكونو مجموعين ونحتافلو كاملين. الله يخليك ليا يا حبيبة باباك، ويحفظك ويخليك ديما فرحانة. كنحبك بزاف بزاف. ❤️🎂`;window.onload=()=>setTimeout(()=>{loading.style.display='none';main.style.display='flex';},2000);gift.onclick=()=>{setTimeout(()=>{main.style.display='none';message.style.display='flex';let i=0;let t=setInterval(()=>{text.textContent+=msg[i]||'';i++;if(i>=msg.length)clearInterval(t);},35);},500)};
+alert("JavaScript works!");
+
+const msg=`بنتي الحبيبة 🤍، باباك كيبغيك بزاف. ما تتصوريش شحال أنا فرحان حيث نتي معايا. والله كنتمنا ليك عيد ميلاد زوين، وإن شاء الله المرة الجاية غادي نكونو مجموعين ونحتافلو كاملين. الله يخليك ليا يا حبيبة باباك، ويحفظك ويخليك ديما فرحانة. كنحبك بزاف بزاف. ❤️🎂`;
+
+window.onload=()=>setTimeout(()=>{
+    loading.style.display='none';
+    main.style.display='flex';
+},2000);
+
+gift.onclick=()=>{
+    setTimeout(()=>{
+        main.style.display='none';
+        message.style.display='flex';
+        let i=0;
+        let t=setInterval(()=>{
+            text.textContent+=msg[i]||'';
+            i++;
+            if(i>=msg.length) clearInterval(t);
+        },35);
+    },500)
+};
